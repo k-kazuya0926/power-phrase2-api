4ad8b323c3491e5a55e72487b3f64a1d640e63b3
Kazuya Kobayashi <kobakazu3686@gmail.com>
2020-10-04 19:30:13 +0900

    jsonのキーをスネークケースに変更

--- src/model/user.go
+++ src/model/user.go
@@ -15,7 +15,7 @@ type User struct {
 	Name      string     `json:"name"`
 	Email     string     `json:"email"`
 	Password  string     `json:"password"`
-	ImageURL  string     `json:"imageUrl"`
+	ImageURL  string     `json:"image_url"`
 }
 
 func CreateUser(user *User) {
