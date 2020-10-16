ec436254ff0e5b41eb0ab155aee1f4ec3d6e4cb0
Kazuya Kobayashi <kobakazu3686@gmail.com>
2020-10-17 06:57:40 +0900

    古い内容を削除

--- src/domain/model/user_model.go
+++ src/domain/model/user_model.go
@@ -7,8 +7,11 @@ import (
 // User struct
 type User struct {
 	ID        int        `json:"id" gorm:"primary_key"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
+	CreatedAt time.Time  `json:"created_at" gorm:"not null;default:current_timestamp"`
+	UpdatedAt time.Time  `json:"updated_at" gorm:"not null;default:current_timestamp"`
 	DeletedAt *time.Time `json:"deleted_at"`
-	Name      string     `json:"name" gorm:"type:varchar(256);not null"`
+	Name      string     `json:"name" gorm:"type:varchar(256);not null;default:''"`
+	Email     string     `json:"email" gorm:"type:varchar(256);not null;default:''"`
+	Password  string     `json:"password" gorm:"type:varchar(256);not null;default:''"`
+	ImageURL  string     `json:"image_url" gorm:"type:varchar(256);not null;default:''"`
 }
