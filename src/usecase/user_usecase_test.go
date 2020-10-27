b18ea569657d9034ba6774256e912fbfd7f363b2
Kazuya Kobayashi <kobakazu3686@gmail.com>
2020-10-28 08:15:39 +0900

    testifyのmock導入

--- src/usecase/user_usecase_test.go
+++ src/usecase/user_usecase_test.go
@@ -8,56 +8,44 @@ import (
 
 	"github.com/k-kazuya0926/power-phrase2-api/domain/model"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"golang.org/x/crypto/bcrypt"
 )
 
 // Mock
 type mockUserRepository struct {
-	returnsError bool
+	mock.Mock
 }
 
-func (ur *mockUserRepository) Create(user *model.User) error {
-	if ur.returnsError {
-		return errors.New("error")
+func (repository *mockUserRepository) Create(user *model.User) error {
+	return repository.Called(user).Error(0)
+}
+
+func (repository *mockUserRepository) FetchByEmail(email string) (*model.User, error) {
+	args := repository.Called(email)
+	user := args.Get(0)
+	if user == nil {
+		return nil, args.Error(1)
 	}
-
-	return nil
+	return user.(*model.User), args.Error(1)
 }
 
-func (ur *mockUserRepository) FetchByEmail(email string) (*model.User, error) {
-	if ur.returnsError {
-		return nil, errors.New("error")
+func (repository *mockUserRepository) FetchByID(id int) (*model.User, error) {
+	args := repository.Called(id)
+	user := args.Get(0)
+	if user == nil {
+		return nil, args.Error(1)
 	}
-
-	return getMockUserForRead(1), nil
+	return user.(*model.User), args.Error(1)
 }
 
-func (ur *mockUserRepository) FetchByID(id int) (*model.User, error) {
-	if ur.returnsError {
-		return nil, errors.New("error")
-	}
-
-	return getMockUserForRead(id), nil
+func (repository *mockUserRepository) Update(user *model.User) error {
+	return repository.Called(user).Error(0)
 }
 
-func (ur *mockUserRepository) Update(user *model.User) error {
-	if ur.returnsError {
-		return errors.New("error")
-	}
-
-	return nil
+func (repository *mockUserRepository) Delete(id int) error {
+	return repository.Called(id).Error(0)
 }
-
-func (ur *mockUserRepository) Delete(id int) error {
-	if ur.returnsError {
-		return errors.New("error")
-	}
-
-	return nil
-}
-
-var ur = mockUserRepository{}
-var uc = NewUserUseCase(&ur)
 
 // 入力用ユーザー
 func getMockUserForInput(id int) *model.User {
@@ -82,62 +70,109 @@ func getMockUserForRead(id int) *model.User {
 
 // ユーザー登録テスト
 func TestCreateUser_success(t *testing.T) {
-	user := getMockUserForInput(1)
+	// 1. Setup
+	repository := mockUserRepository{}
+	usecase := NewUserUseCase(&repository)
+	id := 1
+	user := getMockUserForInput(id)
+	repository.On("Create", mock.AnythingOfType("*model.User")).Return(nil)
 
-	// Assertions
-	assert.NoError(t, uc.CreateUser(user.Name, user.Email, user.Password, user.ImageURL))
+	// 2. Exercise
+	err := usecase.CreateUser(user.Name, user.Email, user.Password, user.ImageURL)
+
+	// 3. Verify
+	assert.NoError(t, err)
+
+	// 4. Teardown
 }
 
 func TestCreateUser_error(t *testing.T) {
-	ur.returnsError = true
-	user := getMockUserForInput(1)
+	// 1. Setup
+	repository := mockUserRepository{}
+	usecase := NewUserUseCase(&repository)
+	id := 1
+	user := getMockUserForInput(id)
+	repository.On("Create", mock.AnythingOfType("*model.User")).Return(errors.New("error"))
 
-	// Assertions
-	assert.Error(t, uc.CreateUser(user.Name, user.Email, user.Password, user.ImageURL))
+	// 2. Exercise
+	err := usecase.CreateUser(user.Name, user.Email, user.Password, user.ImageURL)
 
-	ur.returnsError = false
+	// 3. Verify
+	assert.Error(t, err)
+
+	// 4. Teardown
 }
 
 // ログインテスト
 func TestLogin_success(t *testing.T) {
-	user := getMockUserForInput(1)
-	token, err := uc.Login(user.Email, user.Password)
+	// 1. Setup
+	repository := mockUserRepository{}
+	usecase := NewUserUseCase(&repository)
+	id := 1
+	userForInput := getMockUserForInput(id)
+	userForRead := getMockUserForRead(id)
+	repository.On("FetchByEmail", userForInput.Email).Return(userForRead, nil)
 
-	// Assertions
+	// 2. Exercise
+	token, err := usecase.Login(userForInput.Email, userForInput.Password)
+
+	// 3. Verify
 	assert.NoError(t, err)
 	assert.NotEqual(t, "", token)
+
+	// 4. Teardown
 }
 
 func TestLogin_error_invalidPassword(t *testing.T) {
-	user := getMockUserForInput(1)
-	token, err := uc.Login(user.Email, "invalid")
+	// 1. Setup
+	repository := mockUserRepository{}
+	usecase := NewUserUseCase(&repository)
+	id := 1
+	userForInput := getMockUserForInput(id)
+	userForRead := getMockUserForRead(id)
+	repository.On("FetchByEmail", userForInput.Email).Return(userForRead, nil)
 
-	// Assertions
+	// 2. Exercise
+	token, err := usecase.Login(userForInput.Email, "invalid")
+
+	// 3. Verify
 	assert.Error(t, err)
 	assert.Equal(t, "", token)
+
+	// 4. Teardown
 }
 
 func TestLogin_error_repositoryError(t *testing.T) {
-	ur.returnsError = true
+	// 1. Setup
+	repository := mockUserRepository{}
+	usecase := NewUserUseCase(&repository)
+	id := 1
+	userForInput := getMockUserForInput(id)
+	repository.On("FetchByEmail", userForInput.Email).Return(nil, errors.New("error"))
 
-	user := getMockUserForInput(1)
-	token, err := uc.Login(user.Email, user.Password)
+	// 2. Exercise
+	token, err := usecase.Login(userForInput.Email, userForInput.Password)
 
-	// Assertions
+	// 3. Verify
 	assert.Error(t, err)
 	assert.Equal(t, "", token)
 
-	ur.returnsError = false
+	// 4. Teardown
 }
 
 // ユーザー詳細テスト
 func TestGetUser_success(t *testing.T) {
+	// 1. Setup
+	repository := mockUserRepository{}
+	usecase := NewUserUseCase(&repository)
 	id := 1
 	expected := getMockUserForRead(id)
+	repository.On("FetchByID", id).Return(expected, nil)
 
-	user, err := uc.GetUser(id)
+	// 2. Exercise
+	user, err := usecase.GetUser(id)
 
-	// Assertions
+	// 3. Verify
 	assert.NoError(t, err)
 	assert.Equal(t, expected.ID, user.ID)
 	assert.Equal(t, expected.Name, user.Name)
@@ -145,54 +180,89 @@ func TestGetUser_success(t *testing.T) {
 	assert.Equal(t, expected.CreatedAt, user.CreatedAt)
 	assert.Equal(t, expected.UpdatedAt, user.UpdatedAt)
 	assert.Equal(t, expected.DeletedAt, user.DeletedAt)
+
+	// 4. Teardown
 }
 
 func TestGetUser_error(t *testing.T) {
-	ur.returnsError = true
+	// 1. Setup
+	repository := mockUserRepository{}
+	usecase := NewUserUseCase(&repository)
 	id := 1
-	user, err := uc.GetUser(id)
+	repository.On("FetchByID", id).Return(nil, errors.New("error"))
 
-	// Assertions
+	// 2. Execise
+	user, err := usecase.GetUser(id)
+
+	// 3. Verify
 	assert.Error(t, err)
 	assert.Empty(t, user)
 
-	ur.returnsError = false
+	// 4. Teardown
 }
 
 // ユーザー更新テスト
 func TestUpdateUser_success(t *testing.T) {
+	// 1. Setup
+	repository := mockUserRepository{}
+	usecase := NewUserUseCase(&repository)
 	id := 1
 	user := getMockUserForInput(id)
+	repository.On("Update", mock.AnythingOfType("*model.User")).Return(nil)
 
-	// Assertions
-	assert.NoError(t, uc.UpdateUser(id, user.Name, user.Email, user.Password, user.ImageURL))
+	// 2. Exercise
+	err := usecase.UpdateUser(id, user.Name, user.Email, user.Password, user.ImageURL)
+
+	// 3. Verify
+	assert.NoError(t, err)
+
+	// 4. Teardown
 }
 
 func TestUpdateUser_error(t *testing.T) {
-	ur.returnsError = true
+	repository := mockUserRepository{}
+	usecase := NewUserUseCase(&repository)
 	id := 1
 	user := getMockUserForInput(id)
+	repository.On("Update", mock.AnythingOfType("*model.User")).Return(errors.New("error"))
 
-	// Assertions
-	assert.Error(t, uc.UpdateUser(id, user.Name, user.Email, user.Password, user.ImageURL))
+	// 2. Exercise
+	err := usecase.UpdateUser(id, user.Name, user.Email, user.Password, user.ImageURL)
 
-	ur.returnsError = false
+	// 3. Verify
+	assert.Error(t, err)
+
+	// 4. Teardown
 }
 
 // ユーザー削除テスト
 func TestDeleteUser_success(t *testing.T) {
+	// 1. Setup
+	repository := mockUserRepository{}
+	usecase := NewUserUseCase(&repository)
 	id := 1
+	repository.On("Delete", id).Return(nil)
 
-	// Assertions
-	assert.NoError(t, uc.DeleteUser(id))
+	// 2. Exercise
+	err := usecase.DeleteUser(id)
+
+	// 3. Verify
+	assert.NoError(t, err)
+
+	// 4. Teardown
 }
 
 func TestDeleteUser_error(t *testing.T) {
-	ur.returnsError = true
+	repository := mockUserRepository{}
+	usecase := NewUserUseCase(&repository)
 	id := 1
+	repository.On("Delete", id).Return(errors.New("error"))
 
-	// Assertions
-	assert.Error(t, uc.DeleteUser(id))
+	// 2. Exercise
+	err := usecase.DeleteUser(id)
 
-	ur.returnsError = false
+	// 3. Verify
+	assert.Error(t, err)
+
+	// 4. Teardown
 }
