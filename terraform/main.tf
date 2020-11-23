dee7710cce5986d08775b7acecaf327952656ca6
Kazuya Kobayashi <kobakazu3686@gmail.com>
2020-11-23 17:46:25 +0900

    root_block_device削除

--- terraform/main.tf
+++ terraform/main.tf
@@ -152,10 +152,6 @@ resource "aws_instance" "web" {
   vpc_security_group_ids      = [aws_security_group.app.id]
   subnet_id                   = aws_subnet.public.id
   associate_public_ip_address = "true"
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = "8"
-  }
   tags = {
     Name = "tf_instance"
   }
